2bf1d9a9109872dc4e2a96112a6ced794241d1a7
BlattTV <107757041+BlattTV@users.noreply.github.com>
2024-12-04 13:05:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,10 +36,10 @@ document.querySelectorAll('.webcam-container img').forEach(img => {
 setInterval(() => updateWebcam('webcam1', 'https://www.julianpopp.de/wfc/wfc.jpg'), 6000); // Aktualisiert Webcam 1 alle 1 Minuten
 setInterval(() => updateWebcam('webcam2', 'https://www.skiarea-heubach.com/wp-content/uploads/2023/12/skiarea-cam.jpg'), 6000); // Aktualisiert Webcam 2 alle 1 Minuten
 setInterval(() => updateWebcam('webcam3', 'https://skiarena3.7cam.eu/webcam.jpg'), 6000); // Aktualisiert Webcam 3 alle 1 Minuten
-setInterval(() => updateWebcam('webcam4', 'https://vcdn.bergfex.at/webcams/archive.new/downsized/5/3045/2024/12/04/3045_2024-12-04_0930_688d47e0ed941b8b.jpg'), 6000); // Aktualisiert Webcam 4 alle 1 Minuten
+setInterval(() => updateWebcam('webcam4', 'https://webcams.masserberg.de/turmcam.jpg'), 6000); // Aktualisiert Webcam 4 alle 1 Minuten
 
 // Initiale Aktualisierung
 updateWebcam('webcam1', 'https://www.julianpopp.de/wfc/wfc.jpg');
 updateWebcam('webcam2', 'https://www.skiarea-heubach.com/wp-content/uploads/2023/12/skiarea-cam.jpg');
 updateWebcam('webcam3', 'https://skiarena3.7cam.eu/webcam.jpg');
-updateWebcam('webcam4', 'https://vcdn.bergfex.at/webcams/archive.new/downsized/5/3045/2024/12/04/3045_2024-12-04_0930_688d47e0ed941b8b.jpg');
+updateWebcam('webcam4', 'https://webcams.masserberg.de/turmcam.jpg');
